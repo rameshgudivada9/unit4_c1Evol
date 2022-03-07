6b6ad71a37953f4fa7abeb7b6acc0a63aa9f3e4e
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-03-07 12:42:14 +0530

    2nd commit

--- secondLogger.js
+++ secondLogger.js
@@ -1,14 +1,15 @@
 
+
 const express=require("express");
 
 const app=express();
-
-app.use(logger);
 
 app.get("/books",(req,res)=>{
     // console.log("/books");
     return res.send([{ route: "/books"}])
 });
+app.use(checkPermission);
+
 app.get("/libraries",(req,res)=>{
     // console.log("/libraries");
     return res.send([{route: "/libraries", permission: "true"}])
@@ -24,7 +25,7 @@ app.get("/authors",(req,res)=>{
 
 
 
-function logger(req,res,next){
+function checkPermission(req,res,next){
     console.log("before");
     next();
     console.log("after");
